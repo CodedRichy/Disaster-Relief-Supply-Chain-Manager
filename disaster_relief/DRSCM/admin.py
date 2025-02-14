0c9307b942a73b4086758190ec524701cd12539e
Thomman276 <24cs203@mgits.ac.in>
2025-02-14 23:10:49 +0530

    admin py

--- disaster_relief/DRSCM/admin.py
+++ disaster_relief/DRSCM/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register()
+admin.site.register(UserProfile)
+admin.site.register(ReliefCenter)
+admin.site.register(Disaster)
+admin.site.register(SupplyRequest)
+admin.site.register(Logistics)
+admin.site.register(Notification)
