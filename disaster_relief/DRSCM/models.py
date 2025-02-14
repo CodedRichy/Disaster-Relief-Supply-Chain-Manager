5fafb4261236858e5bcec91ce8f183f70ba01bba
aldr1n7 <24CS029@mgits.ac.in>
2025-02-14 17:28:17 +0530

    .

--- disaster_relief/DRSCM/models.py
+++ disaster_relief/DRSCM/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class disaster(models.Model):
-    id = models.DecimalField(max_digits=5,decimal_places=0)
+#class disaster(models.Model):
+    #id = models.DecimalField(max_digits=5,decimal_places=0)
