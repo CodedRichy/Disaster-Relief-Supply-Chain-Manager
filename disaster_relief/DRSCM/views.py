10fc143f4616b12751e034ab1d2fcfd4897f8ceb
Thomman276 <24cs203@mgits.ac.in>
2025-02-14 12:35:32 +0530

    views 2

--- disaster_relief/DRSCM/views.py
+++ disaster_relief/DRSCM/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def error404(request):
-    return render(request,'index.html')
+    return render(request,'404.html')
 
 def about(request):
     return render(request,'about.html')
@@ -10,5 +10,20 @@ def about(request):
 def contact(request):
     return render(request,'contact.html')
 
-def (request):
+def index(request):
     return render(request,'index.html')
+
+def open(request):
+    return render(request,'open.html')
+
+def price(request):
+    return render(request,'price.html')
+
+def service(request):
+    return render(request,'service.html')
+
+def team(request):
+    return render(request,'team.html')
+
+def testimonial(request):
+    return render(request,'testimonial.html')
